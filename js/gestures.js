80284d5e9dc088d6dc68d91afcc2655a46a7f3b8
abraham-santos <abraham.santos@live.com>
2017-10-01 01:34:16 +0100

    Gestures
    Gestures examples

--- js/gestures.js
+++ js/gestures.js
@@ -2,11 +2,35 @@
 var app = {
 	
 	inicio: function(){
+		
+		this.iniciaBotones();
+		this.iniciaFastClick();
+		this.iniciaHammer();
+		
+	},
+	
+	iniciaBotones: function(){
 		var botonClaro = document.querySelector('#claro');
 		var botonOscuro = document.querySelector('#oscuro');
 		
 		botonClaro.addEventListener('click', this.ponloClaro, false);
 		botonOscuro.addEventListener('click', this.ponloOscuro, false);
+	}
+	
+	iniciaFastClick: function(){
+		FastClick.attach(document.body);
+	}
+	
+	iniciaHammer: function(){
+		var zona = document.getElementById('zona-gestos');
+		var hammertime = new Hammer(zona);
+		
+		hammertime.get('pinch').set({ enable: true});
+		hammertime.get('rotate').set({ enable: true});
+		
+		hammertime.on('tap doubletap swipe press rotate', function(ev){
+			document.querySelector('#info').innerHTML = ev.type+' !';
+		});
 	},
 	
 	ponloClaro: function(){
@@ -21,7 +45,6 @@ var app = {
 
 if('addEventListener' in document){
 	document.addEventListener('DOMContentLoaded', function(){
-		FastClick.attach(document.body);
 		app.inicio();
 	}, false);
 }
